68919bc25616f7ec906337c7d72fa9ddcde2308b
yteja <yteja@DESKTOP-70UUORM.fios-router.home>
2020-03-22 17:11:46 -0500

    MVC

---
+++ src/main/java/com/programcreek/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,27 @@
+package com.programcreek.helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+
+	String message = "Hi";
+	String message1 = "! Welcome to Spring MVC!";
+
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		mv.addObject("message1",message1);
+		return mv;
+	}
+
+}
